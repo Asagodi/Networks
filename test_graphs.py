21d408d2c36b9d894f31b7c8e972bcd1425f934d
Asagodi <abel_sagodi@hotmail.com>
2016-01-01 23:46:26 +0100

    Update test_graphs.py

--- test_graphs.py
+++ test_graphs.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 import random
 %matplotlib inline  
 
+
 G = create_erdos(10, 0.2)
-print G.matrix
-
 TG = nx.DiGraph(G.matrix)
+draw(TG, pos=nx.spring_layout(TG))
 
+
+G = create_watts(10, 0.5)
+TG = nx.DiGraph(G.matrix)
 draw(TG, pos=nx.spring_layout(TG))
