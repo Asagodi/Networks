ad2abe9bd74d03210d1e38f840459ae1366ae646
Asagodi <abel_sagodi@hotmail.com>
2016-01-01 17:03:24 +0100

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -1,8 +1,30 @@
 import numpy
 
+class Node(Object):
+  """ 
+  Useful for the opinion network.
+  """
+  def __init__(self, value = 0):
+    value = value
+
+
+
 #Create three possibilities to represent graph?
 class Graph(Object):
-  __init__(self):
-  nodes = []
-  edges = []
-  matrix = 
+  def __init__(self, matrix = None, nnodes = 1):
+    if matrix != None:
+      self.matrix = matrix
+      #self.nodes = 
+      #self.edges = 
+    else:
+      self.nodes = []
+      self.edges = []
+      #self.matrix = Graph.convertomatrix
+  
+  
+  
+#Creating different graphs
+def create_erdos(n = 1, p = 1.):
+  # n Number of Nodes, p Choice to get edge
+  
+  for 
