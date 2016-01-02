6d67cf8bbb3424ad328bcaefe951cc27f86a3d6d
Asagodi <abel_sagodi@hotmail.com>
2016-01-02 14:30:35 +0100

    Update test_graphs.py

--- test_graphs.py
+++ test_graphs.py
@@ -1,19 +1,24 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import graphs
+import types
 %matplotlib inline  
 
-#G = create_erdos(5, 0.5)
-#print G.matrix
+#G = create_erdos(0, 0.5)
 #TG = nx.Graph(G.matrix)
 #draw(TG, pos=nx.spring_layout(TG))
 
-#G = create_watts(10, 0.5)
-#print G.matrix
+#G = create_watts(1, 0.5)
 #TG = nx.Graph(G.matrix)
 #draw(TG, pos=nx.spring_layout(TG))
 
-G = create_barabasi(10, 1)
+G = create_barabasi(10, 2)
 TG = nx.Graph(G.matrix)
 draw(TG, pos=nx.spring_layout(TG))
+
+G.matrix_to_nodedict()
+G.find_path(0,10)
+G.find_all_paths(5,4)
+G.find_shortest_path(3,7)
 
