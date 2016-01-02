397b4fb274a668ebf9c2aea32642627745481c9f
Asagodi <abel_sagodi@hotmail.com>
2016-01-02 12:08:14 +0100

    Update test_graphs.py

--- test_graphs.py
+++ test_graphs.py
@@ -1,15 +1,19 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-import random
 %matplotlib inline  
 
+#G = create_erdos(5, 0.5)
+#print G.matrix
+#TG = nx.Graph(G.matrix)
+#draw(TG, pos=nx.spring_layout(TG))
 
-G = create_erdos(10, 0.2)
-TG = nx.DiGraph(G.matrix)
+#G = create_watts(10, 0.5)
+#print G.matrix
+#TG = nx.Graph(G.matrix)
+#draw(TG, pos=nx.spring_layout(TG))
+
+G = create_barabasi(10, 1)
+TG = nx.Graph(G.matrix)
 draw(TG, pos=nx.spring_layout(TG))
 
-
-G = create_watts(10, 0.5)
-TG = nx.DiGraph(G.matrix)
-draw(TG, pos=nx.spring_layout(TG))
