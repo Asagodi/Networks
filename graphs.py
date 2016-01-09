e940b80e48a7802b398e1361d9edfa35c700b7de
Asagodi <abel_sagodi@hotmail.com>
2016-01-09 19:16:32 +0100

    VIS SIR

--- graphs.py
+++ graphs.py
@@ -24,7 +24,7 @@ class Graph(object):
                 self.matrix = self.matrix + self.matrix.T - np.diag(self.matrix.diagonal())
             self.nnodes = matrix.shape[0]
             self.matrix_to_edgelist()
-            self.nodedict = self.matrix_to_nodedict()
+            self.matrix_to_nodedict()
             self.nodes = range(self.matrix.shape[0])
 #             self.weightdict = 
         elif nodes == []:
@@ -39,7 +39,7 @@ class Graph(object):
             self.edgedict = dict(zip(self.edges, edgevalues))
             self.matrix = np.zeros((nnodes,nnodes))
             self.edgelist_to_matrix()
-            self.nodedict = self.matrix_to_nodedict()
+            self.matrix_to_nodedict()
             self.nodes = range(self.matrix.shape[0])
         if nodedict != {}:
             self.nodedict = nodedict
@@ -51,26 +51,40 @@ class Graph(object):
         for edge in self.edges:
             self.matrix[edge[0], edge[1]] = self.edgedict[edge]
             
-    def matrix_to_edgelist(self):
+    def matrix_to_edgelist(self, edgevalues = []):
         #Converts the matrix representation to a list of tuples representing the edges.
         #unnecessary?
         ind = np.nonzero(self.matrix)
         fnodes, tnodes = ind[0].astype(long), ind[1].astype(long)
 #         fnodes, tnodes = ind[0], ind[1]
         self.edges =  zip(fnodes, tnodes)
+        if edgevalues == []:
+            edgevalues = [1] * len(self.edges)
+        self.edgedict = dict(zip(self.edges, edgevalues))
 
     def matrix_to_nodedict(self):
-        nodedict = {}
+        self.nodedict = {}
         for i in range(self.nnodes):
             ind = np.nonzero(self.matrix[i,:])[0].tolist()
-            nodedict[i] = ind
-        return nodedict
+            self.nodedict[i] = ind
     
     def nodedict_to_edgelist(self):
         self.edges = []
         for start in self.nodedict.keys():
             for end in self.nodedict[start]:
                 self.edges.append((start, end))
+                
+    def update_graph(self, from_rep):
+        #Update other representations from from_rep = "matrix", "edgelist" or "nodedict"
+        if from_rep == "matrix":
+            self.matrix_to_edgelist()
+            self.matrix_to_nodedict()
+        elif from_rep == "edgelist":
+            self.edgelist_to_matrix()
+            self.matrix_to_nodedict()
+        elif from_rep == "nodedict":
+            self.nodedict_to_edgelist()
+            self.edgelist_to_matrix()
         
     def add_node(self, name, connections):
         #name can be number, connections as list
@@ -83,9 +97,12 @@ class Graph(object):
         
     def remove_node(self, node):
         assert node in self.nodes, "Node not in graph"
-        self.nodedict.pop(node, None)
-        #update other representations
-        
+        self.matrix[node,:] = 0
+        self.matrix[:, node] = 0
+        self.nodes.remove(node)
+        #update other representations, if only working with nodedict/paths not necessary
+        self.update_graph(from_rep = "matrix")
+    
     def remove_edge(self, node):
         0
         
@@ -104,6 +121,19 @@ class Graph(object):
         for i in self.nodes:
             ad += self.degree(i)
         return ad/float(len(self.nodes))
+    
+    def is_connected(self):
+        #checks if there is a connection between all pairs
+        #brute force, should be possible to make it more efficient
+        pairs = [ (i,j) for i in self.nodes for j in self.nodes]
+        for (start,end) in pairs:
+            paths = self.find_all_paths(start,end)
+            if paths == []:
+                return False
+        return True
+    
+    def find_node_highest_degree(self):
+        return np.argmax(self.all_degrees())
         
     def find_path(self, start, end, path=[]):
         assert start in self.nodes, "Start is not a node in the graph"
@@ -176,6 +206,7 @@ class Graph(object):
         return c/float(len(self.nodes))
     
     def degree_dict(self):
+        #returns numer of nodes which have a certain degree for each possible degree as a dict
         dd = {}
         for node in self.nodes:
             degree = self.degree(node)
@@ -186,12 +217,20 @@ class Graph(object):
         return dd
     
     def degree_list(self):
+        #returns numer of nodes which have a certain degree for each possible degree as a list
         dl = [0]*self.nnodes
         for node in self.nodes:
             dl[self.degree(node)] +=1
         return dl
     
-    def SIR(self, time = 1, n_inf = 1, p = 1., sick_time = 5):
+    def all_degrees(self):
+        dl = [0]*self.nnodes
+        #returns the degree of each node as a list in the order as they are in self.nodes
+        for node in self.nodes:
+            dl[node] = self.degree(node)
+        return dl
+    
+    def SIR(self, time = 1, n_inf = 1, p = 1., sick_time = 5, vis = False):
         inf_list = np.zeros((self.nnodes,1))
         recovered_list = np.zeros((self.nnodes,1)) #these nodes are immune
         infected_nodes = np.random.choice(range(self.nnodes), size=n_inf, replace=False)
@@ -200,6 +239,21 @@ class Graph(object):
         susceptible = [self.nnodes - n_inf]
         infected = [n_inf]
         recovered = [0]
+        if vis:
+            colorlist = ['r' if i in np.nonzero(inf_list)[0] else 'b' if i in np.nonzero(recovered_list)[0] else 'y' for i in range(self.nnodes)]
+            plt.ion()
+            G = nx.Graph(self.matrix)
+            pos=nx.spring_layout(G)
+            ##         #draw sick nodes red, recovered nodes blue, susceptible nodes yellow 
+            nx.draw_networkx_nodes(G,pos,
+                           node_color=colorlist,
+                           node_size=500, alpha=0.8)
+            nx.draw_networkx_edges(G,pos,width=1.0,alpha=0.5)
+            plt.axis('off')
+            plt.draw()
+            sleep(1.)
+
+
         for t in range(time):
             #update sickness times
             inf_time[np.nonzero(inf_list)] += 1
@@ -216,6 +270,17 @@ class Graph(object):
                         sick = True
                 if sick and recovered_list[i] == 0:
                     inf_list[i] = 1
+            if vis:
+                colorlist = ['r' if i in np.nonzero(inf_list)[0] else 'b' if i in np.nonzero(recovered_list)[0] else 'y' for i in range(self.nnodes)]
+                nx.draw_networkx_nodes(G,pos,
+                                       node_color=colorlist,
+                                       node_size=500, alpha=0.8)
+                nx.draw_networkx_edges(G,pos,width=1.0,alpha=0.5)
+                plt.axis('off')
+                fig_title = "SIR, time=%d" %t
+#                 plt.savefig(fig_title)
+                plt.show()
+                sleep(1.)
                     
             #update lists
             num_inf = sum(inf_list)
@@ -223,8 +288,28 @@ class Graph(object):
             susceptible.append(self.nnodes - num_inf - num_rec)
             infected.append(sum(inf_list))    
             recovered.append(num_rec)
-            
+        plt.show()
         return susceptible, infected, recovered
+    
+    def failures(self):
+        num_failure = 0
+        while self.is_connected() and len(self.nodes) != 1:
+           
+            self.remove_node(random.choice(self.nodes))
+            num_failure += 1
+            print num_failure
+        return num_failure
+        
+    
+    def attacks(self):
+        num_failure = 0
+        while self.is_connected() or len(self.nodes) == 1:
+           
+            self.remove_node(random.choice(self.nodes))
+            num_failure += 1
+        return num_failure
+    
+    
 
 #Creating different graphs
 def create_erdos(n = 1, p = 1.):
@@ -267,12 +352,12 @@ def create_barabasi(n, n_0):
     #connect first node to 2,3...,n_0+1
     matrix[0,1:n_0+1] = 1
     #connect (n_0+2)th to 2,3...,n_0+1  (still not important which)
-    matrix[1:n_0+1, n_0+1] = 1
-    for new in range(n_0+2, n):
-        nodes = range(n)
-        nodes.remove(new)
-        probs = list(matrix.sum(axis=1)+matrix.sum(axis=0))
-        probs.pop(new)
+#     matrix[1:n_0+1, n_0+1] = 1
+    for new in range(n_0+1, n):
+        nodes = range(new)
+        #nodes.remove(new)
+        probs = list(matrix.sum(axis=1)+matrix.sum(axis=0))[:new]
+        #probs.pop(new)
         probs = probs/sum(probs)
         ind = np.random.choice(nodes, size=n_0, replace=False, p=probs)
         matrix[ind, new] = 1
